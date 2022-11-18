37662e819a388fb51c32e7a353ce09d7319356b7
Chris <chris.cybula@gmail.com>
2022-11-18 16:20:10 +0100

    Configure global styling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createGlobalStyle } from 'styled-components';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: gray;
+  }
+`;
+
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
+
