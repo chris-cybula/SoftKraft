2eaa0c301bae996b45bb2ae65ff6e2b003e7da14
Chris <chris.cybula@gmail.com>
2022-11-18 15:35:56 +0100

    API integration

--- src/App.js
+++ src/App.js
@@ -1,4 +1,25 @@
+import axios from 'axios';
+import { useEffect } from "react"
+
 function App() {
+
+  useEffect(() => {
+    getMovies()
+  })
+
+  const getMovies = async () => {
+    try {
+      const res = await axios.get(
+        `https://www.omdbapi.com/?i=tt3896198&apikey=98f4ba6b`
+      );
+
+      console.log(res)
+
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="App">
       <p>SoftKraft</p>
