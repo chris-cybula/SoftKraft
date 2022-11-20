597e8b433a607ac7296eb168dbaac8c09af6f83b
Chris <chris.cybula@gmail.com>
2022-11-20 12:55:47 +0100

    Add movie input component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import { Button, TextField } from '@mui/material';
-import axios from 'axios';
 import { useState } from "react"
 import Header from "./components/Header"
 import Messages from './components/Messages';
 import MovieInfo from './components/MovieInfo';
+import MovieInput from './components/MovieInput';
 import styled from "styled-components"
 
 
@@ -19,88 +18,17 @@ const ContentContainer = styled.div`
     margin: 2rem 0;
   }
 `
-
-const Input = styled.div`
-  width: 300px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`
-
-const SearchButton = styled(Button)`
- &.searchButton {
-  background-image: linear-gradient(90deg,#288cbb,#74e2c5);
-  height: 37px;
-
-    &:hover {
-      background-image: linear-gradient(90deg,#288cbb,#74e2c5);
-    }
-  }
-`
-
-const TextFieldInput = styled(TextField)`
-  &.error {
-    label {
-      color: #d32f2f;
-    }
-
-    fieldset {
-      border-color: #d32f2f;
-    }
-  }
-`
-
 function App() {
-  const [movie, setMovie] = useState('')
   const [movieTitle, setMovieTitle] = useState('')
   const [movieYear, setMovieYear] = useState('')
   const [movieThumbnail, setMovieThumbnail] = useState('')
   const [errorState, setErrorState] = useState(false)
 
-  const getMovie = async () => {
-    const apikey = 'tt3896198&apikey=98f4ba6b'
-
-    if (movie !== "") {
-      try {
-        const res = await axios.get(
-          `https://www.omdbapi.com/?i=${apikey}&t=${movie}`
-        );
-
-        if (res.data['Response'] !== 'False') {
-          setMovieTitle(res.data['Title'])
-          setMovieYear(res.data['Year'])
-          setMovieThumbnail(res.data['Poster'])
-          setErrorState(false)
-        } else {
-          setMovieTitle('')
-          setMovieYear('')
-          setMovieThumbnail('')
-          setErrorState(true)
-        }
-
-      } catch (err) {
-        console.log(err)
-      }
-    } else {
-      setErrorState(true)
-    }
-  };
-
-  const inputHandler = event => {
-    setMovie(event.target.value);
-  };
-
   return (
     <div>
       <Header />
       <ContentContainer>
-        <Input>
-          <TextFieldInput className={errorState === true ? 'error' : ''} id="outlined-basic" label="Your movie..." variant="outlined" onChange={inputHandler}
-            value={movie} />
-          <SearchButton variant="contained" onClick={getMovie} className={'searchButton'}>Search</SearchButton>
-        </Input>
-
-        
+        <MovieInput setMovieTitle={setMovieTitle} setMovieYear={setMovieYear} setMovieThumbnail={setMovieThumbnail} setErrorState={setErrorState} errorState={errorState}/>
         <MovieInfo movieTitle={movieTitle} movieYear={movieYear} movieThumbnail={movieThumbnail} errorState={errorState} />
         <Messages movieTitle={movieTitle} movieYear={movieYear} movieThumbnail={movieThumbnail} errorState={errorState} />
       </ContentContainer>
---
+++ src/components/MovieInput.js
@@ -0,0 +1,81 @@
+import { useState } from "react"
+import styled from "styled-components"
+import { Button, TextField } from '@mui/material';
+import axios from 'axios';
+
+const Input = styled.div`
+  width: 300px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const SearchButton = styled(Button)`
+ &.searchButton {
+  background-image: linear-gradient(90deg,#288cbb,#74e2c5);
+  height: 37px;
+
+    &:hover {
+      background-image: linear-gradient(90deg,#288cbb,#74e2c5);
+    }
+  }
+`
+
+const TextFieldInput = styled(TextField)`
+  &.error {
+    label {
+      color: #d32f2f;
+    }
+
+    fieldset {
+      border-color: #d32f2f;
+    }
+  }
+`
+
+function MovieInput({setMovieTitle, setMovieYear, setMovieThumbnail, errorState, setErrorState}) {
+  const [movie, setMovie] = useState('')
+
+  const getMovie  = async () => {
+    const apikey = 'tt3896198&apikey=98f4ba6b'
+
+    if (movie !== "") {
+      try {
+        const res = await axios.get(
+          `https://www.omdbapi.com/?i=${apikey}&t=${movie}`
+        );
+
+        if (res.data['Response'] !== 'False') {
+          setMovieTitle(res.data['Title'])
+          setMovieYear(res.data['Year'])
+          setMovieThumbnail(res.data['Poster'])
+          setErrorState(false)
+        } else {
+          setMovieTitle('')
+          setMovieYear('')
+          setMovieThumbnail('')
+          setErrorState(true)
+        }
+
+      } catch (err) {
+        console.log(err)
+      }
+    } else {
+      setErrorState(true)
+    }
+  };
+
+  const inputHandler = event => {
+    setMovie(event.target.value);
+  };
+
+  return (
+    <Input>
+      <TextFieldInput className={errorState === true ? 'error' : ''} id="outlined-basic" label="Your movie..." variant="outlined" onChange={inputHandler}
+        value={movie} />
+      <SearchButton variant="contained" onClick={getMovie} className={'searchButton'}>Search</SearchButton>
+    </Input>
+  );
+}
+
+export default MovieInput;
