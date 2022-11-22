7a8abadacb4239ba93a68270b8e7d3c105ba1f9d
Chris <chris.cybula@gmail.com>
2022-11-22 12:58:23 +0100

    Fix global style

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,8 @@ import styled, { createGlobalStyle } from "styled-components"
 
 const GlobalStyle = createGlobalStyle`
   body {
-    @import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap');
-
     background-color: #f8f9fa;
     margin: 0;
-    font-family: 'Lato', sans-serif;
     color: #444;
   }
 `;
